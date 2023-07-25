643580433e44c9d488ad764cbee8e49e339cbe1d
Milena Santos <milena.oliveira@grupointegrado.br>
2023-07-25 13:52:14 -0300

    skils e projetos

--- components/Projetos.vue
+++ components/Projetos.vue
@@ -1,80 +1,91 @@
 <template>
     <section>
-        <h1>Projetos</h1>
-
-        <div class="card-container">
-            <div class="card">
-                <b-card title="Título">
-                    <div class="linguagens">
-                        <b-card-text class="botao">
-                            Linguagem 1
-                        </b-card-text>
-
-                        <b-card-text class="botao">
-                            Linguagem 2
-                        </b-card-text>
-
-                        <b-card-text class="botao">
-                            Linguagem 3
-                        </b-card-text>
-                    </div>
-
-                    <b-card-text class="descricao">
-                        Descrição
-                    </b-card-text>
-
-                    <b-card-text>
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
-                    </b-card-text>
-
-                    <b-img src="../static/img.jpeg" class="card-image"></b-img>
-                
-                    <b-button class="repositorio">Visualizar repositório</b-button>
-                </b-card>
+      <h1>Projetos</h1>
+  
+      <div class="card-container">
+        <div v-for="(card, index) in getCurrentPageCards" :key="index" class="card">
+          <b-card :title="card.title">
+            <div class="linguagens">
+              <b-card-text v-for="(language, i) in card.languages" :key="i" class="botao">
+                {{ language }}
+              </b-card-text>
             </div>
+  
+            <b-card-text class="descricao">
+              {{ card.description }}
+            </b-card-text>
+  
+            <b-card-text>
+              {{ card.content }}
+            </b-card-text>
 
-            <div class="card">
-                <b-card title="Título">
-                    <div class="linguagens">
-                        <b-card-text class="botao">
-                            Linguagem 1
-                        </b-card-text>
-
-                        <b-card-text class="botao">
-                            Linguagem 2
-                        </b-card-text>
-
-                        <b-card-text class="botao">
-                            Linguagem 3
-                        </b-card-text>
-                    </div>
-
-                    <b-card-text class="descricao">
-                        Descrição
-                    </b-card-text>
-
-                    <b-card-text>
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
-                    </b-card-text>
-
-                    <b-img src="../static/img.jpeg" class="card-image"></b-img>
-                
-                    <b-button class="repositorio">Visualizar repositório</b-button>
-                </b-card>
-            </div>
+           <!-- <b-img :src="card.image" :alt="card.title" class="card-image" :key="currentPage"></b-img>-->
+  
+            <b-button class="repositorio">Visualizar repositório</b-button>
+          </b-card>
         </div>
+      </div>
+
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="totalCards"
+        :per-page="perPage"
+        aria-controls="card-container"
+      ></b-pagination>
     </section>
-</template>
-
-<script>
-export default {
-    name: "Projetos"
-}
-</script>
-
-<style scoped>
-
-section {
+  </template>
+  
+  <script>
+  export default {
+    name: "Projetos",
+    data() {
+      return {
+        perPage: 1,
+        currentPage: 1,
+        cards: [
+          {
+            title: "Card 1",
+            languages: ["Linguagem 1", "Linguagem 2", "Linguagem 3"],
+            description: "Descrição do Card 1",
+            content: "Conteúdo do Card 1",
+          //  image: require("../static/img.jpeg"),
+          },
+          {
+            title: "Card 2",
+            languages: ["Linguagem 4", "Linguagem 5", "Linguagem 6"],
+            description: "Descrição do Card 2",
+            content: "Conteúdo do Card 2"
+          },
+          {
+            title: "Card 3",
+            languages: ["Linguagem 7", "Linguagem 8", "Linguagem 9"],
+            description: "Descrição do Card 3",
+            content: "Conteúdo do Card 3"
+          },
+          {
+            title: "Card 4",
+            languages: ["Linguagem 10", "Linguagem 11", "Linguagem 12"],
+            description: "Descrição do Card 4",
+            content: "Conteúdo do Card 4"
+          },
+        ],
+      };
+    },
+    computed: {
+      totalCards() {
+        return this.cards.length;
+      },
+      getCurrentPageCards() {
+        const startIndex = (this.currentPage - 1) * this.perPage;
+        const endIndex = startIndex + this.perPage;
+        return this.cards.slice(startIndex, endIndex);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+ section {
     background-color: #15132b;
 }
 
@@ -144,4 +155,10 @@ h1{
     background-color: #241c45;
     border-color: #8d60db;
 }
-</style>
+
+ul.pagination.b-pagination{
+ color: #8d60db;
+ justify-content: center;
+ padding-bottom: 50px;
+}
+</style> 
--- components/Skils.vue
+++ components/Skils.vue
@@ -1,95 +1,115 @@
 <template>
     <section>
-        <h1>Skils</h1>
-        <hr>
-
-        <div class="skils">
-            <div class="barras">
-                <p>Linguagem 1</p>
-                <div class="progress w-50 mb-2">
-                    <div role="progressbar" class="progress-bar">75%</div>
-                </div>
-
-                <p>Linguagem 2</p>
-                <div class="progress w-50 mb-2">
-                    <div role="progressbar" class="progress-bar">75%</div>
-                </div>
-
-                <p>Linguagem 3</p>
-                <div class="progress w-50 mb-2">
-                    <div role="progressbar" class="progress-bar">75%</div>
-                </div>
-
-                <p>Linguagem 4</p>
-                <div class="progress w-50 mb-2">
-                    <div role="progressbar" class="progress-bar">75%</div>
-                </div>
-            </div>
-
-            <div class="texto">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt urna sit amet massa pharetra aliquam a nec elit. 
-                        Pellentesque non vulputate ipsum. Donec interdum ante ante, tempor dictum orci tempor sed. Curabitur sit amet lectus vel eros 
-                        malesuada aliquet. Nulla pharetra nisl erat, et placerat sem malesuada vel. Nullam quis semper neque, a elementum tortor. Nam 
-                        placerat pharetra maximus. Nullam faucibus elementum ex porttitor porta. Fusce venenatis consequat blandit. In mauris mi, 
-                        porta eget pharetra non, tincidunt vitae dolor. Fusce lacinia viverra urna. Quisque malesuada nulla non nunc feugiat, id 
-                        ullamcorper ligula sagittis. Quisque a blandit ligula.</p>
-            </div>
+      <h1>Skills</h1>
+      <hr />
+  
+      <div class="skils">
+        <div class="barras">
+          <p>Linguagem 1</p>
+          <div class="progress w-100 mb-2">
+            <div role="progressbar" class="progress-bar" style="width: 75%;">75%</div>
+          </div>
+  
+          <p>Linguagem 2</p>
+          <div class="progress w-100 mb-2">
+            <div role="progressbar" class="progress-bar" style="width: 75%;">75%</div>
+          </div>
+  
+          <p>Linguagem 3</p>
+          <div class="progress w-100 mb-2">
+            <div role="progressbar" class="progress-bar" style="width: 75%;">75%</div>
+          </div>
+  
+          <p>Linguagem 4</p>
+          <div class="progress w-100 mb-2">
+            <div role="progressbar" class="progress-bar" style="width: 75%;">75%</div>
+          </div>
         </div>
-
-        <div class="d-flex">
-          <b-button variant="outline-light" class="ml-auto">
-            <b-img src="../static/seta.png"></b-img>
-          </b-button>
+  
+        <div class="texto">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            tincidunt urna sit amet massa pharetra aliquam a nec elit. Pellentesque
+            non vulputate ipsum. Donec interdum ante ante, tempor dictum orci tempor
+            sed. Curabitur sit amet lectus vel eros malesuada aliquet. Nulla pharetra
+            nisl erat, et placerat sem malesuada vel. Nullam quis semper neque, a
+            elementum tortor. Nam placerat pharetra maximus. Nullam faucibus elementum
+            ex porttitor porta. Fusce venenatis consequat blandit. In mauris mi,
+            porta eget pharetra non, tincidunt vitae dolor. Fusce lacinia viverra
+            urna. Quisque malesuada nulla non nunc feugiat, id ullamcorper ligula
+            sagittis. Quisque a blandit ligula.
+          </p>
         </div>
-  </section>
-</template>
-
-<script>
-export default {
-    name: "Skils"
-}
-</script>
-
-
-<style scoped>
-section{
+      </div>
+  
+      <div class="d-flex">
+        <b-button variant="outline-light" class="ml-auto">
+          <b-img src="../static/seta.png"></b-img>
+        </b-button>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: "Skills",
+  };
+  </script>
+  
+  <style scoped>
+  section {
     background-color: white;
-}
-
-h1 {
+  }
+  
+  h1 {
     color: #8d60db;
     text-align: center;
     font-size: 50px;
     margin-top: 3%;
-}
-
-hr{
+  }
+  
+  hr {
     border: none;
     border-top: 2px solid #8d60db;
     margin: 3% 8% 5%;
-}
-
-.progress-bar {
+  }
+  
+  .progress-bar {
     background-color: #8d60db !important;
     width: 75%;
-}
+  }
+  
+  .skils {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 10px;
+    align-items: flex-start;
+  }
+  
+  .barras {
+    margin: auto;
+    width: 100%;
+    max-width: 600px;
+    padding: 0 20px;
+    margin-top: 40px;
+  }
+  
+  .texto {
+    margin: auto;
+    max-width: 600px;
+    padding: 0 20px;
+    margin-top: 50px;
+  }
+  
 
-.skils {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 10px;
-  align-items: flex-start;
-}
-
-
-.barras {
-  margin: auto;
-  width: 100%;
-  margin-left: 20%;
-}
-
-.texto {
-  margin: auto;
-  margin-right: 20%;
-}
-</style>
+  @media (max-width: 768px) {
+    .skils {
+      grid-template-columns: 1fr;
+    }
+  
+    .barras,
+    .texto {
+      padding: 0 10px;
+    }
+  }
+  </style>
