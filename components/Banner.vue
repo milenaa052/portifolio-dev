db824fc7fb6f7e8c1cf4e1ba3aab3fcf6b945b8e
Milena Santos <milena.oliveira@grupointegrado.br>
2023-08-04 13:51:44 -0300

    banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,54 +1,102 @@
 <template>
-  <div class="main-banner">
-      <p class="texto1">Olá, meu nome é</p>
-
-      <p class="texto2">Milena Santos de Oliveira</p>
-
-      <p class="texto3">Eu sou</p>
+  <div class="max-width">
+    <div class="main-banner">
+      <div class="text-wrapper">
+        <p class="texto1">Olá, meu nome é</p>
+        <p class="texto2">Milena Santos de Oliveira</p>
+        <p class="texto3">Eu sou <span class="texto4" v-html="paper"></span></p>
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script>
 export default {
-  name: "Banner"
+  name: "Banner",
+  data(){
+    return{
+      text: "Desenvolvedora Front-end.",
+      paper: '',
+      timer: null
+    }
+  },
+  mounted(){
+    this.printer()
+  },
+  methods:{
+    printer(){
+      this.stoper()
+      let i = -1
+        this.timer = setInterval(() =>{
+          i++
+          const x = i%this.text.length
+          this.paper += this.text[x]
+        }, 300-(Math.random()*50))
+    },
+    stoper(){
+      clearInterval(this.timer)
+      this.paper = ''
+    }
+  },
+  watch:{
+    paper:function(){
+      if(this.paper.length == this.text.length){
+        this.paper = ''
+      }
+    }
+  }
 };
-
-/*var typed = new Typed('.typing', {
-    strings: ['Desenvolvedor Front-end', 'Desenvolvedor Backend'],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });*/
 </script>
 
 <style scoped>
+.max-width{
+  width: 100%;
+  display: flex;
+  margin-left: 3%;
+}
+
 .main-banner {
   background-size: cover;
   background-position: center;
   position: relative;
   display: flex;
   justify-content: flex-start;
+  font-family: 'Ubuntu', sans-serif;
 }
 
-.texto1{
+.text-wrapper{
+  padding-top: 45%;
+}
+
+.main-banner .texto1{
   color: white;
-  margin-left: 30px;
-  margin-top: 310px;
   font-size: 35px;
 }
 
-.texto2{
+.main-banner .texto2{
   color: white;
-  margin-left: -270px;
-  margin-top: 340px;
+  font-weight: 600;
   font-size: 55px;
 }
 
-.texto3{
+.main-banner .texto3{
   color: white;
-  margin-left: -40%;
-  margin-top: 410px;
+  margin: 5px 0;
   font-size: 35px;
+}
+
+.main-banner .texto4{
+  display: inline;
+  border-right: 2px white solid;
+  animation: typebar 0.s steps(40) infinite;
+  color: white;
+   font-weight: 500;
+}
+
+@keyframes typebar{
+  from{border-right: 2px white solid;}
+  to{border-right: 2px transparent solid;}
 }
 
 @media screen and (max-width: 768px) {
@@ -56,26 +104,6 @@ export default {
     height: 400px;
     flex-direction: column;
     align-items: flex-start;
-  }
-
-  .texto1 {
-    margin-top: 130px;
-    text-align: left;
-    font-size: 25px;
-    margin-left: 10px;
-  }
-
-  .texto2 {
-    margin-top: -10px;
-    text-align: left;
-    margin-left: 10px;
-  }
-
-  .texto3 {
-   margin-left: 12px;
-    text-align: left;
-    font-size: 30px;
-    margin-top: -13px;
   }
 }
 
